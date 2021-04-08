4fe3e73b2f59f990f88d4f73328c4215378d9ce4
NedloG <daniil20020928@gmail.com>
2021-04-08 15:25:44 +0300

    v1.3.1 -  Database connection fix attempt 1

--- src/config.ts
+++ src/config.ts
@@ -3,23 +3,29 @@ import { Questions } from './entity/Questions';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
 
-export const sqliteConfig:SqliteConnectionOptions = {
-    type: "sqlite",
-    database: "database.db",
-    entities: [Users, Questions],
-    synchronize: true,
-    logging: false
+export const sqliteConfig: SqliteConnectionOptions = {
+  type: "sqlite",
+  database: "database.db",
+  entities: [Users, Questions],
+  synchronize: true,
+  logging: false
 }
 
-export const postgresConfig:PostgresConnectionOptions = {
-    type: "postgres",
-    host: "ec2-54-155-226-153.eu-west-1.compute.amazonaws.com",
+export const postgresConfig: PostgresConnectionOptions = {
+  type: "postgres",
+    /*host: "ec2-54-155-226-153.eu-west-1.compute.amazonaws.com",
     port: 5432,
     username: "ugljkacrcymoqn",
     password: process.env.POSTGRES_PASSWORD,
-    database: "d2cpa1v5m6ul8d",
-    synchronize: true,
-    logging: false,
-    entities: [Users, Questions],
-    ssl:true
+    database: "d2cpa1v5m6ul8d",*/
+  url: process.env.DATABASE_URL,
+  synchronize: true,
+  logging: false,
+  entities: [Users, Questions],
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
 }
